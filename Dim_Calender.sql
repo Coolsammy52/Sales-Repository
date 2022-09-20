aae4404f1a0630293cf861959b669ba87f63d858
Coolsammy52 <98569843+Coolsammy52@users.noreply.github.com>
2022-09-20 12:05:07 +0100

    Add files via upload

---
+++ Dim_Calender.sql
@@ -0,0 +1,26 @@
+--Cleansed Dim_DateTable --
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  --,[DayNumberOfWeek],
+  [EnglishDayNameOfWeek] AS Day, 
+  --,[SpanishDayNameOfWeek],
+  --,[FrenchDayNameOfWeek],
+  --,[DayNumberOfMonth],
+  --,[DayNumberOfYear],
+  --,[WeekNumberOfYear],
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS Monthshort, 
+  --Useful for front end date navigation and front end graphs,
+  --,[SpanishMonthName],
+  --,[FrenchMonthName],
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year --[CalendarSemester],
+  --,[FiscalQuarter],
+  --,[FiscalYear],
+  --,[FiscalSemester]
+FROM 
+  [dbo].[DimDate] 
+WHERE 
+  CalendarYear >= 2019
